9a2366a0d01ed745a1519e5a4a9388379380f4ab
RobertGarabetian <robertgarabetian90@gmail.com>
2024-11-18 22:30:14 -0800

    first commit

---
+++ backend/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	// "io"
+	"log"
+	// "net"
+	"net/http"
+	// "net/url"
+	// "strings"
+)
+
+func main() {
+	http.HandleFunc("/dog", handleDogRequest)
+	// http.HandleFunc("/", handleRequest)
+	log.Println("Starting proxy server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleDogRequest(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	breed := req.URL.Query().Get("breed")
+	if breed == "" {
+		http.Error(w, "Breed not specified", http.StatusBadRequest)
+		return
+	}
+
+	apiBreed := formatBreedName(breed)
+
+	imageUrl, err := fetchDogImage(apiBreed)
+	if err != nil {
+		http.Error(w, "Error fetching dog image: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"imageUrl": imageUrl,
+	})
+
+}
+func formatBreedName(breed string) string {
+	// Convert breed name to lowercase and replace spaces with hyphens
+	breed = strings.ToLower(breed)
+	breed = strings.ReplaceAll(breed, " ", "-")
+	return breed
+}
+func fetchDogImage(breed string) (string, error) {
+	apiUrl := "https://dog.ceo/api/breed/" + breed + "/images/random"
+	fmt.Println(apiUrl)
+	resp, err := http.Get(apiUrl)
+	fmt.Println(resp)
+
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", err
+	}
+	var result struct {
+		Message string `json:"message"`
+		Status  string `json:"status"`
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return "", err
+	}
+
+	if result.Status != "success" {
+		return "", err
+	}
+	return result.Message, nil
+
+}
+
+// func handleRequest(w http.ResponseWriter, req *http.Request) {
+// 	if req.Method == http.MethodConnect {
+// 		handleHTTPS(w, req)
+// 	} else {
+// 		handleHTTP(w, req)
+// 	}
+// }
+// func handleHTTPS(w http.ResponseWriter, req *http.Request) {
+// 	log.Printf("HTTPS Request: %s", req.Host)
+
+// 	destConn, err := net.Dial("tcp", req.Host)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+// 	}
+// 	w.WriteHeader(http.StatusOK)
+// 	hijacker, ok := w.(http.Hijacker)
+// 	if !ok {
+// 		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+// 		return
+// 	}
+// 	clientConn, _, err := hijacker.Hijack()
+
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+// 		return
+// 	}
+// 	go transfer(destConn, clientConn)
+// 	go transfer(clientConn, destConn)
+// }
+
+// func transfer(destination net.Conn, source net.Conn) {
+// 	defer destination.Close()
+// 	defer source.Close()
+// 	io.Copy(destination, source)
+// }
+
+// func handleHTTP(w http.ResponseWriter, req *http.Request) {
+// 	log.Printf("HTTP Request: %s %s", req.Method, req.URL.String())
+// 	url, err := url.Parse(req.URL.String())
+// 	if err != nil {
+// 		http.Error(w, "Bad request", http.StatusBadRequest)
+// 	}
+
+// 	proxyReq, err := http.NewRequest(req.Method, url.String(), req.Body)
+// 	if err != nil {
+// 		http.Error(w, "Error creating Proxy request", http.StatusInternalServerError)
+// 	}
+// 	proxyReq.Header = req.Header
+// 	proxyReq.Header.Add("X-Proxy-Server", "GoProxy")
+
+// 	client := http.Client{}
+// 	resp, err := client.Do(proxyReq)
+// 	if err != nil {
+// 		http.Error(w, "Error forwarding request", http.StatusInternalServerError)
+// 	}
+// 	defer resp.Body.Close()
+
+// 	for key, values := range resp.Header {
+// 		for _, value := range values {
+// 			w.Header().Add(key, value)
+// 		}
+// 	}
+// 	w.WriteHeader(resp.StatusCode)
+// 	io.Copy(w, resp.Body)
+
+// 	bodyBytes, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		http.Error(w, "Error reading response body", http.StatusInternalServerError)
+// 		return
+// 	}
+// 	resp.Body.Close()
+
+// 	bodyString := string(bodyBytes)
+// 	bodyString = strings.ReplaceAll(bodyString, "Hello", "Hi")
+// 	w.Write([]byte(bodyString))
+
+// }
---
+++ dog-photo-app/src/App.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./App.css";
+function App() {
+  const [imageUrl, setImageUrl] = useState("");
+
+  const breeds = [
+    "Australian Shepherd",
+    "Dachshund",
+    "Pitbull",
+    "Golden Retriever",
+    "African",
+  ];
+
+  function handleClick(breed: string) {
+    // Send a request to the proxy server
+    fetch(`http://localhost:8080/dog?breed=${encodeURIComponent(breed)}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setImageUrl(data.imageUrl);
+        console.log(data.imageUrl);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching image:", error);
+      });
+  }
+
+  return (
+    <div className="w-screen h-screen bg-blue-500 grid grid-cols-2">
+      <div className="col-span-1 p-4 items-center justify-center flex flex-col">
+        <ul className="space-y-3">
+          {breeds.map((breed) => (
+            <li key={breed}>
+              <button
+                className="w-full border p-3"
+                onClick={() => handleClick(breed)}
+              >
+                {breed}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="col-span-1 flex flex-col items-center justify-center p-4">
+        <img src={imageUrl} alt="dog" className="max-w-full max-h-full" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
